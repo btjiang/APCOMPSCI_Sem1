4653d435e33098e5ced1842ff3f8cd4b1a34f09c
Brittany Jiang <brittanytjiang@gmail.com>
2016-09-02 10:52:45 -0700

    Added Files

---
+++ APLesson_01/ASCIIone.java
@@ -0,0 +1,34 @@
+public class firstProg 
+{
+	public static void main (String[]args)
+	{
+		System.out.println("  $  $$$  $  ")
+		System.out.println("  $ $   $ $  ")
+		System.out.println("  $$     $$  ")
+		System.out.println("  $       $  ")
+		System.out.println(" $$       $$ ")
+		System.out.println("$ $       $ $")
+		System.out.println(" $$       $$ ")
+		System.out.println("  $$      $  ")
+		System.out.println("  $ $     $  ")
+		System.out.println("  $  $    $  ")
+		System.out.println("  $   $   $  ")
+		System.out.println("  $    $  $  ")
+		System.out.println("  $     $  ")
+		System.out.println("  $     $$ ")
+		System.out.println("  $     $ $")
+		System.out.println("  $     $$ ")
+		System.out.println("  $     $  ")
+		System.out.println("  $    $$  ")
+		System.out.println("  $   $ $  ")
+		System.out.println("  $  $  $  ")
+		System.out.println("  $ $   $  ")
+		System.out.println("  $$    $  ")
+		System.out.println("  $     $  ")
+		System.out.println(" $$     $  ")
+		System.out.println("  $     $  ")
+		System.out.println("")
+		System.out.println("")
+	}
+
+} 
