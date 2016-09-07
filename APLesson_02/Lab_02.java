72144cc1fd92025634b1ac12f67cfd80b5519b1f
Brittany Jiang <brittanytjiang@gmail.com>
2016-09-07 12:13:37 -0700

    added lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,36 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 53;
+		int b = 17;
+		int product = a * b;
+		
+		
+		System.out.println(a + " multiplied by " + b + " is "  + product);
+		
+		System.out.println(" ");
+		
+		String first = "1234";
+		String second = " Mulberry Lane ";
+		
+		System.out.println(first + second);
+		
+		String third = "Chicago";
+		String fourth = ", Illinois";
+		String fifth = " 56789";
+		
+		System.out.println(third + fourth + fifth);
+		
+		System.out.println(" ");
+		
+		int l = 13
+		int w = 16
+		int h = 9
+		int recArea = (2*l*w)+ (2*w*h) + (2*l*h);
+		
+		System.out.println(The )
+	    
+	}
+}
+
