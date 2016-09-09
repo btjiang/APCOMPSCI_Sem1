8b69baa23e5ec7192a361ed238d844a6a6dee80b
Brittany Jiang <brittanytjiang@gmail.com>
2016-09-09 11:29:04 -0700

    finished ASCIIone

--- APLesson_01/ASCIIone.java
+++ APLesson_01/ASCIIone.java
@@ -1,34 +1,34 @@
-public class firstProg 
+public class ASCIIone 
 {
 	public static void main (String[]args)
 	{
-		System.out.println("  $  $$$  $  ")
-		System.out.println("  $ $   $ $  ")
-		System.out.println("  $$     $$  ")
-		System.out.println("  $       $  ")
-		System.out.println(" $$       $$ ")
-		System.out.println("$ $       $ $")
-		System.out.println(" $$       $$ ")
-		System.out.println("  $$      $  ")
-		System.out.println("  $ $     $  ")
-		System.out.println("  $  $    $  ")
-		System.out.println("  $   $   $  ")
-		System.out.println("  $    $  $  ")
-		System.out.println("  $     $  ")
-		System.out.println("  $     $$ ")
-		System.out.println("  $     $ $")
-		System.out.println("  $     $$ ")
-		System.out.println("  $     $  ")
-		System.out.println("  $    $$  ")
-		System.out.println("  $   $ $  ")
-		System.out.println("  $  $  $  ")
-		System.out.println("  $ $   $  ")
-		System.out.println("  $$    $  ")
-		System.out.println("  $     $  ")
-		System.out.println(" $$     $  ")
-		System.out.println("  $     $  ")
-		System.out.println("")
-		System.out.println("")
+		System.out.println("  $$$$$$$$$$$$$$$$$$$$$$$$$ ");
+		System.out.println("  $$$$$$$$$$$$$$$$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$");
+		System.out.println("                 $$$$$$$$$$$");
+		System.out.println("                 $$$$$$$$$$$");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$  ");
+		System.out.println("                 $$$$$$$$$$$  ");
+		System.out.println("                 $$$$$$$$$$$  ");
+		System.out.println("                 $$$$$$$$$$$  ");
+		System.out.println("                 $$$$$$$$$$$  ");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$   ");
+		System.out.println("                 $$$$$$$$$$$  ");
+		System.out.println("                 $$$$$$$$$$$ ");
+		System.out.println("                 $$$$$$$$$$$");
+		System.out.println("                 $$$$$$$$$$$");
+		System.out.println("$$$$$$      $$$$$$$$$$$");
+		System.out.println("$$$$$$     $$$$$$$$$$$ ");
+		System.out.println("  $$$$$$  $$$$$$$$$$$  ");
+		System.out.println("   $$$$$$$$$$$$$$$$$   ");
+		System.out.println("    $$$$$$$$$$$$$$$  ");
+		System.out.println("     $$$$$$$$$$$$$     ");
+		System.out.println("      $$$$$$$$$$$       ");
 	}
-
 } 
