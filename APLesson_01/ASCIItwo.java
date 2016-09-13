c872d4993b00f17e371f26cbb8a15525e72dcc6b
btjiang <brittanytjiang@gmail.com>
2016-09-13 12:12:17 -0700

    Started ASCII two

--- APLesson_01/ASCIItwo.java
+++ APLesson_01/ASCIItwo.java
@@ -2,5 +2,9 @@ public class ASCIItwo
 {
 	public static void main (String[]args)
 	{
-		System.out.println(" ")
+		System.out.println("               *               ");
+		System.out.println("              * *              ");
+		System.out.println("             *   *             ");
+		
+		
 	}
