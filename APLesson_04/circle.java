50d658b1e1b133a3c335fc3c6e8e69d6925e2a74
btjiang <JiangB8966@sduhsd.lan>
2016-09-29 11:18:27 -0700

    Finish circle

---
+++ APLesson_04/circle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class circle
+{
+	
+	static double r;
+	static double area;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter the radius length of your circle:");
+		r = kb.nextDouble();
+		calcArea();
+		print();
+	}
+	
+	public static void calcArea()
+	{
+		area = 3.14 * (r * r);
+	}
+	
+	public static void print()
+	{
+		System.out.printf("The area of a circle with a radius of " + r + " is %10.5f.", area);
+	}
+}
