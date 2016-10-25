875db408d4c73daef4b8db11993d24b45592b1fc
btjiang <JiangB8966@sduhsd.lan>
2016-10-25 11:08:17 -0700

    Finished LLesson Five and up to Triangle in Six

--- APLesson_05/Password.java
+++ APLesson_05/Password.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
+
 public class Password
 {
-	public static void main{String[]args}
+	static Scanner kb;
+	
+	public static void main(String[]args)
+	{
+		String password = "Candle";
+		String username = "BillNye";
+		passcheck();
+	}
+		
+	public static void passcheck()
+	{
+		kb = new Scanner(System.in);
+		System.out.println("Please enter your username:");
+		String un = kb.next();
+		System.out.println("Please enter your password:");
+		String pw = kb.next();
+		if (un.equals("BillNye")&&pw.equals("Candle"))
 		{
-			
+			System.out.println("You are granted access!");
 		}
+		
+		else if (!(un.equals("BillNye"))&&(pw.equals("Candle")))
+		{
+			System.out.println("Your username is incorrect!");
+			passcheck();
+		}
+		else if ((un.equals("BillNye"))&&!(pw.equals("Candle")))
+		{
+			System.out.println("Your password is incorrect!");
+			passcheck();
+		}
+		
+		else 
+		{
+			System.out.println("Your username and password are incorrect!");
+			passcheck();
+		}
+	}
+	
 }
