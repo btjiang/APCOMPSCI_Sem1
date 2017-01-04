c79d2f8cbb05d0342f70f9a2292067bcd999ad41
btjiang <JiangB8966@sduhsd.lan>
2017-01-04 12:09:24 -0800

    Finish GameHealth

--- APLesson_11/Gun.java
+++ APLesson_11/Gun.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Gun
 {
-	static int bulletCount = ;
-	static int CLIPSIZE = 16;
-	static int shotCount = ;
-	static String[]clip = new String[];
+	static int bulletCount = 0;
+	static int cz = 16;
+	static int shotCount = 0;
+	static String[]clip = new String[cz];
 	
 	public static void main(String[]args)
 	{
-		
+		bulletCount= 96;
+		shotCount = 0;
+		resetClip();
+		while ( )
 	}
 }
