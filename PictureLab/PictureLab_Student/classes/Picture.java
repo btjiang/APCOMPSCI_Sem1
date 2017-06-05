38ee9051e33fedbf5f09f7875dd94afbaba7256b
btjiang <brittanytjiang@gmail.com>
2017-06-04 21:09:20 -0700

    Update Picture.java

--- PictureLab/PictureLab_Student/classes/Picture.java
+++ PictureLab/PictureLab_Student/classes/Picture.java
@@ -285,9 +285,9 @@ public class Picture extends SimplePicture
    
     Pixel[][] pixels = this.getPixels2D();
     
-    for (int row = 163; row < mirrorPoint; row++)
+    for (int row = 158; row < mirrorPoint; row++)
     {
-      for (int col = 112; col < 168; col++)
+      for (int col = 104; col < 170; col++)
       {
       
         upPixel = pixels[row][col];      
@@ -296,6 +296,25 @@ public class Picture extends SimplePicture
       }
     }
   }
+  
+   public void mirrorGull()
+  {
+    int mirrorPoint = 345;
+    Pixel rightPixel = null;
+    Pixel leftPixel = null;
+    Pixel[][] pixels = this.getPixels2D();   
+     
+    for (int row = 235; row < 323; row++)
+    {
+      for (int col = 238; col < mirrorPoint; col++)
+     {
+        rightPixel = pixels[row][col];      
+        leftPixel = pixels[row][mirrorPoint - col + mirrorPoint/3];
+        leftPixel.setColor(rightPixel.getColor());
+      }
+    }
+  }
+
   /** copy from the passed fromPic to the
     * specified startRow and startCol in the
     * current picture
