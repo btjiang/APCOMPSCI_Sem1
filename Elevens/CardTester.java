5cebdee5ff8d5812bc2137eec2c92bf4a8c12329
btjiang <brittanytjiang@gmail.com>
2017-06-07 20:28:10 -0700

    Create CardTester.java

--- Elevens/CardTester.java
+++ Elevens/CardTester.java
@@ -7,7 +7,14 @@ public class CardTester {
 	 * The main method in this class checks the Card operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public static void main(String[] args)
+	{
+		Card card1 = new Card("ace", "hearts", 5) ;
+		Card card2 = new Card("four", "clubs", 10);
+		Card card3 = new Card("king", "jacks", 7);
+		
+		System.out.println(card1);
+		System.out.println(card2);
+		System.out.println(card3);
 	}
 }
