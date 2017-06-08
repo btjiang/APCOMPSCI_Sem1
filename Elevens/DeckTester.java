c1e2d45454c45afc76e5a36457b24aa652a55a08
btjiang <brittanytjiang@gmail.com>
2017-06-07 20:47:35 -0700

    Create DeckTester.java

--- Elevens/DeckTester.java
+++ Elevens/DeckTester.java
@@ -8,6 +8,10 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
+		String[] suits = {"hearts", "diamonds", "spades", "clubs"};
+		int[] pointValues = {1,2,3,4,5,6,7,8,9,10,10,10,10};
+		Deck d = new Deck(ranks, suits, pointValues);
+		System.out.println(d);
 	}
 }
